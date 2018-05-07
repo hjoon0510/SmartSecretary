f55d12bfa0b418a35332cec9d9a809169b0bcdd4
HyunJun Lim <hjoon0510@gmail.com>
2018-05-07 18:28:36 +0900

    Updated schedule image
    Updated image file to display initial screen
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

---
+++ webpage/schedule.php
@@ -0,0 +1,16 @@
+<?php
+
+// Author: Hyunjoon Lim
+// Title: initial schedule screen
+// Date: May-06-2018
+// Description: This webpage is to display initial screen of user schedule
+
+echo ("<br>");
+echo ("<br>");
+echo ("<br>");
+echo ("<br>");
+echo ("<center>");
+echo ("<img src=./image/schedule.gif width=200 height=50 border=0></img>");
+echo ("</center>");
+?>
+
