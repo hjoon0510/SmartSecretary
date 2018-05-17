891d9874e3798ca7699ab6afb20bb0995a02ede6
HyunJun Lim <hjoon0510@gmail.com>
2018-05-17 23:23:12 +0900

    Display data of current.txt file
    I display data of current.txt file for debugging.
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- motion/pir-final.py
+++ motion/pir-final.py
@@ -11,14 +11,12 @@
 # 1. Check location of  +DC voltage and GND line
 # 2. Change sensor and pulse button (orange color) appropriately
 #
-# How to run:
+# How to run program:
 # $ sudo visudo 
-#
 #  # User privilege specification
-#  root    ALL=(ALL:ALL) ALL
-#  hjoon0510       ALL=NOPASSWD: ALL
-#
-# sudo ./pir-final.py
+#    root            ALL=(ALL:ALL) ALL
+#    hjoon0510       ALL=NOPASSWD: ALL
+# $ ./pir-final.py
 #
 # Reference:
 # 1. http://gpiozero.readthedocs.io/en/stable/recipes.html
@@ -36,24 +34,27 @@ condition = "Rain\n"
 #----------- Do not modify below statements -------------------
 try:
     print "[DEBUG] Starting motion sensor..."
+    # MotionSensor function probes the movement of people.
     pir = MotionSensor(GPIO_PIN)
     while True:
         pir.wait_for_motion()
         count += 1
         print ("[DEBUG] Motion Detected! " + str(count))
-        # check if current weather is "Rain"
         file = open("../webpage/current.txt")
         current = file.read()
-        # if current weather is rainy day, let's play sound.
-        if ( current == condition):
-            print ("[DEBUG] Current weather is %s" % current)
+        print ("[DEBUG] The weather data of curent.txt file is %s." % current)
+        # if current weather is "Rain".
+        if (current == condition):
             cmd = "mplayer ../sound/wma/sound-rain-english.wma"
             os.system(cmd)
         else:
-            print ("[DEBUG] Current weather is %s" % current)
             cmd = "mplayer ../sound/wav/dingdong.wav"
             os.system(cmd)
-        time.sleep(0.1)
+        # TODO: if current weather is "Snow".
+        #
+        # wait for 5 seconds
+        time.sleep(5)
+# let's exit if users press "Ctrl + C".
 except KeyboardInterrupt:
     print "Quit"
     GPIO.cleanup()
