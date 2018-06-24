f0886d84d1fb7a66134e6e703bb432e00bb80b4f
lsy0314 <34805004+lsy0314@users.noreply.github.com>
2018-06-24 11:52:37 +0900

    Update run.c

--- run.c
+++ run.c
@@ -1,5 +1,5 @@
 /**
- * @Author: Hyunjun Lim
+ * @Author: Hyunjun Lim, Suyeon Lim
  * @Date: Jun-17-2018
  * @Title: main program to launch programs
  * @License: Star
@@ -12,15 +12,40 @@
 #include <stdio.h>  // printf()
 #include <stdlib.h> // system()
 
-// Run Smart Secretary
-printf ("Starting Smart Secretary Launcher...");
-printf ("Press CTRL+C if you want to stop.";
+int main(void){
+    int result;
+  
+    // Start Smart Secretary
+    printf ("Starting Smart Secretary Launcher...");
+    printf ("Press CTRL+C if you want to stop.";
 
-// Step1: run Apache web-server
-// $ sudo systemctl restart apache2
-system(sudo systemctl restart apache2);
+    // Step1: run Apache web-server
+    // * $ sudo systemctl restart apache2
+    result = system(sudo systemctl restart apache2);
+    if (result == 0){
+        printf("[ERROR] No shell is available.\n"); exit(0);
+    }
+    else if (result == 127){
+        printf("[ERROR] A web-server process could not be created.\n"); exit(0);
+    }
+    else
+        printf("Apache web-server is successfully started.\n");
 
-// Step2: run PIR motion sensor
-// * with background: exec /var/www/html/motion/pir-sensor.py &> /dev/null &
-// * with foreground: /var/www/html/motion/pir-sensor.py
-system(/var/www/html/motion/pir-sensor.py)
+    // Step2: run PIR motion sensor
+    // * with background: exec /var/www/html/motion/pir-sensor.py &> /dev/null &
+    // * with foreground: /var/www/html/motion/pir-sensor.py
+    result = system(/var/www/html/motion/pir-sensor.py);
+    if (result == 0){
+        printf("[ERROR] No shell is available.\n"); exit(0);
+    }
+    else if (result == 127){
+        printf("[ERROR] A PIR sensor process could not be created.\n"); exit(0);
+    }
+    else
+        printf("A PIR sensor process is successfully started.\n");
+            
+    // display help message to users.
+    printf("Please visit http://localhost/ to use web-application.\n");
+            
+    return 0;
+}
