4e68eb5585609bfa9b74ce0e4cfd06e219c165c1
HyunJun Lim <hjoon0510@gmail.com>
2018-06-24 16:06:40 +0900

    Added run.c to manage programs
    I added run.c to manage program
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- run.c
+++ run.c
@@ -1,12 +1,12 @@
 /**
  * @Author: Hyunjun Lim, Suyeon Lim
  * @Date: Jun-17-2018
- * @Title: main program to launch programs
+ * @Title: Launcher to start apache web-server and pir sensor
  * @License: Star
  * @Prequisites: PHP web-application, Python PIR motion sensor application
  * @Description:
  * - How to compile: $ gcc -o run run.c
- * - How to run: $ run
+ * - How to run:     $ run
  */ 
 
 #include <stdio.h>  // printf()
@@ -16,17 +16,18 @@ int main(void){
     int result;
   
     // Start Smart Secretary
-    printf ("Starting Smart Secretary Launcher...");
-    printf ("Press CTRL+C if you want to stop.";
+    printf ("Starting Smart Secretary Launcher...\n");
+    printf ("Press CTRL+C if you want to stop.\n");
 
     // Step1: run Apache web-server
     // * $ sudo systemctl restart apache2
-    result = system(sudo systemctl restart apache2);
-    if (result == 0){
-        printf("[ERROR] No shell is available.\n"); exit(0);
+    result = system("sudo systemctl daemon-reload");
+    result = system("sudo systemctl restart apache2");
+    if (result == -1){
+        printf("[ERROR] apache2: A web-server process could not be created. return value is %d.\n",result); exit(0);
     }
     else if (result == 127){
-        printf("[ERROR] A web-server process could not be created.\n"); exit(0);
+        printf("[ERROR] apache2: A web-server process could not be executed. return value is %d\n",result); exit(0);
     }
     else
         printf("Apache web-server is successfully started.\n");
@@ -34,12 +35,12 @@ int main(void){
     // Step2: run PIR motion sensor
     // * with background: exec /var/www/html/motion/pir-sensor.py &> /dev/null &
     // * with foreground: /var/www/html/motion/pir-sensor.py
-    result = system(/var/www/html/motion/pir-sensor.py);
-    if (result == 0){
-        printf("[ERROR] No shell is available.\n"); exit(0);
+    result = system("/var/www/html/motion/pir-sensor.py");
+    if (result == -1){
+        printf("[ERROR] pir-sensor: A PIR sensor process could not be created. return value is %d.\n",result); exit(0);
     }
     else if (result == 127){
-        printf("[ERROR] A PIR sensor process could not be created.\n"); exit(0);
+        printf("[ERROR] pir-sensor: A PIR sensor process could not be executed. return value is %d.\n",result); exit(0);
     }
     else
         printf("A PIR sensor process is successfully started.\n");
