0a893dc3a927ae3d2f37941076af91b9aaa00ce0
HyunJun Lim <hjoon0510@gmail.com>
2018-07-12 21:49:11 +0900

    Modify widthxheight of schedule picture
    I modified size of schedule photo
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webpage/schedule.php
+++ webpage/schedule.php
@@ -10,7 +10,8 @@ echo ("<br>");
 echo ("<br>");
 echo ("<center>");
 echo ("<img src=./image/schedule.gif width=200 height=50 border=0></img>");
-echo ("<img src=./image/postit.jpg width=200 height=180 border=0></img>");
+echo ("<br>");
+echo ("<img src=./image/postit.jpg width=250 height=210 border=0></img>");
 echo ("</center>");
 ?>
 
